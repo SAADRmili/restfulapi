77327e1430dfe748ec3af63d755e96890db6231f
SAADRMILI <saadrmili45@gmail.com>
2020-02-08 19:42:24 +0100

    Implementing Complex Operations for Category

--- app/Category.php
+++ app/Category.php
@@ -18,6 +18,10 @@ class Category extends Model
     ];
 
 
+    protected $hidden =[
+        'pivot'
+    ];
+
     public function products(){
      return $this->belongsToMany(Product::class);
     }
